8f91dd7273f7f826b59c08cc78b2073d3a3ccf3f
itoxir <itoxir@gmail.com>
2013-10-24 17:43:40 +0200

    Create hash_code.h

---
+++ hash_code.h
@@ -0,0 +1,13 @@
+// std lib includes
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+// opencv includes
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+using namespace cv;
+
+
+int create_hash_code(vector<int> m, Mat P, Mat thr);
